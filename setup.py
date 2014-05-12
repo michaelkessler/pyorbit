67791a849c0797db9909ed6e0ff104e3bded35e9
Michael Kessler <mike@toadgrass.com>
2014-05-11 23:45:58 -0700

    Initial commit of setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='orbit',
+      version='1.0',
+      description='Python Keplerian Orbit Library',
+      author='Michael Kessler',
+      author_email='mikepkes@gmail.com',
+      packages=['orbit'],
+      package_dir={'orbit':'src'}
+     )
